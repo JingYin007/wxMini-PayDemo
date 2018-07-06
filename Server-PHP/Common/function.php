85e36039e418e3de25ff409009200fb1eb20c295
moTzxx <JingYin007@users.noreply.github.com>
2018-07-06 20:06:29 +0800

    代码完善
    整理完毕 进行代码上传

--- Server-PHP/Common/function.php
+++ Server-PHP/Common/function.php
@@ -68,14 +68,3 @@ function curl_post_raw($url,$rawData){
     return ($data);
 }
 
-/**
- * 只保留字符串首尾字符，隐藏中间用*代替（两个字符时只显示第一个）
- * @param string $user_name 姓名
- * @return string 格式化后的姓名
- */
-function substr_cut($user_name){
-    $strlen     = mb_strlen($user_name, 'utf-8');
-    $firstStr     = mb_substr($user_name, 0, 1, 'utf-8');
-    $lastStr     = mb_substr($user_name, -1, 1, 'utf-8');
-    return $strlen == 2 ? $firstStr . str_repeat('*', mb_strlen($user_name, 'utf-8') - 1) : $firstStr . str_repeat("*", $strlen - 2) . $lastStr;
-}
--- wxChat/utils/config.js
+++ wxChat/utils/config.js
@@ -5,8 +5,8 @@ class Config {
   }
 }
 
-Config.restUrl = 'http://fetowNew.com/WxApi/';
-Config.imgServer = 'http://img.fetow.com/';
+Config.restUrl = 'https://www.myServer.com/WxApi/';
+Config.imgServer = 'http://img.myServer.com/';
 Config.onPay = true;  //是否启用支付
 
 export { Config };
